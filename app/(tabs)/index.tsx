0a8e6254c5674933230c741e37ba910f5d84bd24
Lechcher <lechcher@mail.com>
2026-07-31 03:58:54 +0700

    imple: font

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,26 +4,24 @@ import { Text } from "react-native";
 
 export default function App() {
 	return (
-		<NWSafeAreaView className="flex-1 bg-background p-5">
-			<Text className="text-xl font-bold text-blue-500">
-				Welcome to Nativewind!
-			</Text>
+		<NWSafeAreaView className="flex-1 bg-background p-5 font-sans-regular">
+			<Text className="text-5xl font-sans-extrabold">Home</Text>
 			<Link
 				href={"/onboarding"}
-				className="mt-4 rounded bg-primary text-white p-4"
+				className="mt-4 rounded bg-primary text-white p-4 font-sans-bold"
 			>
 				Go to Onboarding
 			</Link>
 
 			<Link
 				href={"/(auth)/sign-in"}
-				className="mt-4 rounded bg-primary text-white p-4"
+				className="mt-4 rounded bg-primary text-white p-4 font-sans-bold"
 			>
 				Go to Sign In
 			</Link>
 			<Link
 				href={"/(auth)/sign-up"}
-				className="mt-4 rounded bg-primary text-white p-4"
+				className="mt-4 rounded bg-primary text-white p-4 font-sans-bold"
 			>
 				Go to Sign Up
 			</Link>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,39 @@
 import "@/global.css";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useCallback } from "react";
+import { View } from "react-native";
+import "react-native-reanimated";
 
-import { Stack } from "expo-router";
+SplashScreen.preventAutoHideAsync();
+
+export const unstable_settings = {
+	anchor: "(tabs)",
+};
 
 export default function RootLayout() {
-	return <Stack screenOptions={{ headerShown: false }} />;
+	const [fontsLoaded, fontError] = useFonts({
+		"sans-regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+		"sans-bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+		"sans-medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+		"sans-semibold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+		"sans-extrabold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+		"sans-light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+	});
+
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded || fontError) {
+			await SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded, fontError]);
+
+	if (!fontsLoaded && !fontError) {
+		return null;
+	}
+
+	return (
+		<View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+			<Stack screenOptions={{ headerShown: false }} />
+		</View>
+	);
 }
